f73f8d23829289cd09ddb6d358463ab4da07c4e3
Christopher Philp <chrismphilp@hotmail.co.uk>
2020-05-19 16:37:36 +0100

    Updating npm packages

--- src/assets/tattoo-removal/TattooRemoval.data.tsx
+++ src/assets/tattoo-removal/TattooRemoval.data.tsx
@@ -7,7 +7,7 @@ const tattooRemovalData: IPostCollection = {
   "category": "Science",
   "link": "/tattoo-removal",
   "date": "24/06/2019",
-  "widgetImage": "http://www.drnumb.com/blog/wp-content/uploads/2016/10/Post11-Laser-Tattoo-removal.jpg",
+  "widgetImage": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cosmetician-performing-a-laser-depilation-on-a-royalty-free-image-1582657775.jpg?crop=1.00xw:0.751xh;0,0.173xh&resize=980:*",
   "widgetImageAlt": "React Logo",
   "contentKey": "tattooRemoval"
 };
