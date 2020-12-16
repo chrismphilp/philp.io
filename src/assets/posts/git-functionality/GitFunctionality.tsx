cb2eaf7007ad4aa213713b7ab267a94ff8f70612
Christopher Philp <chrismphilp@hotmail.co.uk>
2020-12-16 15:39:15 +0000

    [master] | Adding further topics to be discussed

--- src/assets/posts/git-functionality/GitFunctionality.tsx
+++ src/assets/posts/git-functionality/GitFunctionality.tsx
@@ -10,8 +10,8 @@ const GitFunctionality: FunctionComponent = () => {
 
       <p>
         Git as a version control system has usage across the world. But many developers still
-        limit themselves, staying with mainstay commands. This article will investigate these
-        litany of commands, and hopefully increase your understanding.
+        limit themselves, staying within mainstay commands. This article will investigate these
+        litany of commands, and hopefully increase your understanding and productivity.
       </p>
 
       <h3>Merge</h3>
@@ -36,7 +36,8 @@ const GitFunctionality: FunctionComponent = () => {
       <h3>Rebase</h3>
 
       <p>
-        Similar to merge, <i>rebase</i> is used to integrate changes from one branch, into another.
+        Rebasing is the process of moving or combining a sequence of commits to a new base commit.
+        Similar to merge, <i>rebase</i> can used to integrate changes from one branch, into another.
         However, unlike merge (which is a non-destructive operation), rebase is used to move the
         entire feature branch, to the tip of the master branch, incorporating all of the previous
         commits from the master branch. This removes the need for the unnecessary commits from a
@@ -84,19 +85,51 @@ Date:   Wed Dec 16 14:38:59 2020 +0000
       <p>
           This has needlessly increased the size of the git history, with lots of commits that
         add nothing. In this case it is preferable to combine commits <code className="language-git">d84b3e</code>,
-        <code className="language-git">6a4e34b</code>, and <code className="language-git">bd0a16d</code>.
+        <code className="language-git">6a4e34b</code>, and <code className="language-git">bd0a16d</code>. A commit
+        squash or fix can therefore be performed. This involves moving the commit/s into their parent node in the commit
+        tree. A <b>squash</b> will aggregate the commit messages of the combined commits, whilst a <b>fix</b> will ignore them
+        completely.
+      </p>
+
+      <p>
+        As a side-note, rebasing does not actually alter the already made commits (as this is not possible in git
+        through common commands), it creates new ones whilst removing any references to the old ones.
       </p>
 
       <pre>
         <code className={'language-git'}>
-          {`// Combine all changes after this commit e.g. f392171, ba9dd9a, df71a27
-$ git rebase -i 0db3d898d87770b47a85c56c61a46bc46343e30a
+          {`$ git rebase -i 0db3d898d87770b47a85c56c61a46bc46343e30a
 pick bd0a16d [master] | Altering Git instruction
-pick 6a4e34b [master] | Furthering altering Git instruction
-pick d84b3e1 [master] | Removing unused line
+f 6a4e34b [master] | Furthering altering Git instruction
+f d84b3e1 [master] | Removing unused line
+
+$ git log
+commit dc08bbe95d71a0079f080d326376e2029f11ce3f (HEAD -> master)
+Author: Christopher Philp <email>
+Date:   Wed Dec 16 14:45:46 2020 +0000
+
+    [master] | Altering Git instruction
+
+commit 0db3d898d87770b47a85c56c61a46bc46343e30a
+Author: Christopher Philp <email>
+Date:   Wed Dec 16 14:38:59 2020 +0000
+
+    [master] | Adding initial Git article
 `}
         </code>
       </pre>
+
+      <p>
+        Here we can see from the <i>git log</i> that <code className="language-git">d84b3e</code> and
+        <code className="language-git">6a4e34b</code> have been now moved inside <code className="language-git">d84b3e</code>,
+        inheriting its commit message due to the use of the <b>fix</b> keyword.
+      </p>
+
+      <h3>Reset</h3>
+
+      <h3>Revert</h3>
+
+      https://www.atlassian.com/git/tutorials/resetting-checking-out-and-reverting
 
     </SubjectPage>
   );
