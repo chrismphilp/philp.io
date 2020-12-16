dc08bbe95d71a0079f080d326376e2029f11ce3f
Christopher Philp <chrismphilp@hotmail.co.uk>
2020-12-16 14:45:46 +0000

    [master] | Altering Git instruction

--- src/assets/posts/git-functionality/GitFunctionality.tsx
+++ src/assets/posts/git-functionality/GitFunctionality.tsx
@@ -54,23 +54,49 @@ const GitFunctionality: FunctionComponent = () => {
       <pre>
         <code className={'language-git'}>
           {`$ git log
-* df71a27 - (HEAD feature_x) Updated CSS for new elements (4 minutes ago)
-* ba9dd9a - Added new elements to page design (15 minutes ago)
-* f392171 - Added new feature X (1 day ago)
-* d7322aa - (origin/feature_x) Proof of concept for feature X (3 days ago)
+commit d84b3e1f899d48c49e8f513302293ff36d1b0421 (HEAD -> master)
+Author: Christopher Philp <email>
+Date:   Wed Dec 16 14:46:45 2020 +0000
 
-// Combine all changes after this commit e.g. f392171, ba9dd9a, df71a27
-$ git rebase -i d7322aa
-pick f392171 Added new feature X
-pick ba9dd9a Added new elements to page design
-pick df71a27 Updated CSS for new elements
+    [master] | Removing unused line
+
+commit 6a4e34b35dd3d5cd6e3fe34df9e04b3f80be0b85
+Author: Christopher Philp <email>
+Date:   Wed Dec 16 14:46:06 2020 +0000
+
+    [master] | Furthering altering Git instruction
+
+commit bd0a16d1ffbaba26aa2a3e3715eba19503b4c190
+Author: Christopher Philp <email>
+Date:   Wed Dec 16 14:45:46 2020 +0000
+
+    [master] | Altering Git instruction
+
+commit 0db3d898d87770b47a85c56c61a46bc46343e30a
+Author: Christopher Philp <email>
+Date:   Wed Dec 16 14:38:59 2020 +0000
+
+    [master] | Adding initial Git article
 `}
         </code>
       </pre>
 
       <p>
-
+          This has needlessly increased the size of the git history, with lots of commits that
+        add nothing. In this case it is preferable to combine commits <code className="language-git">d84b3e</code>,
+        <code className="language-git">6a4e34b</code>, and <code className="language-git">bd0a16d</code>.
       </p>
+
+      <pre>
+        <code className={'language-git'}>
+          {`// Combine all changes after this commit e.g. f392171, ba9dd9a, df71a27
+$ git rebase -i 0db3d898d87770b47a85c56c61a46bc46343e30a
+pick bd0a16d [master] | Altering Git instruction
+pick 6a4e34b [master] | Furthering altering Git instruction
+pick d84b3e1 [master] | Removing unused line
+`}
+        </code>
+      </pre>
 
     </SubjectPage>
   );
