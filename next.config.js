f0c9ddcd2984b4fa153610c7a0e5ec366971cc47
Christopher Philp <christopherph@monday.com>
2025-10-17 15:13:12 +0100

    Adding PostHog to blog deployment for analytics

--- next.config.js
+++ next.config.js
@@ -42,5 +42,19 @@ module.exports = {
   ...withPWA(mdx),
   publicRuntimeConfig: {
     version,
-  }
-}
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+};
